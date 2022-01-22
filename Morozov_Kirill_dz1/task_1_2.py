b6c41f2494b1cd74c6b8dcfdc7fbbe90aaff9e2f
kirill020291 <oooamg.snab@gmail.com>
2022-01-23 01:32:45 +0500

    решение задачи о сумме кубов нечетных чисел от 1 до 1000

--- Morozov_Kirill_dz1/task_1_2.py
+++ Morozov_Kirill_dz1/task_1_2.py
@@ -1,4 +1,4 @@
-def sum_list_1(dataset):
+def sum_list(dataset):
     res = 0
     while dataset != 0:
         res += dataset % 10
@@ -8,8 +8,8 @@ def sum_list_1(dataset):
 
 my_list = [i ** 3 for i in range(1, 1001, 2)]
 
-result_1 = sum(filter(lambda num: sum_list_1(num) % 7 == 0, my_list))
+result_1 = sum(filter(lambda num: sum_list(num) % 7 == 0, my_list))
 print(result_1)
 
-result_2 = sum(filter(lambda num: sum_list_1(num + 17) % 7 == 0, my_list))
+result_2 = sum(filter(lambda num: sum_list(num + 17) % 7 == 0, my_list))
 print(result_2)
