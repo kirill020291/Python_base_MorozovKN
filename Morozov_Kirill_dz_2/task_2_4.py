2a1db5d999b89d7d6cc93b2f74eead65d8dcff75
kirill020291 <oooamg.snab@gmail.com>
2022-01-25 02:02:55 +0500

    задача с искаженными данными

--- Morozov_Kirill_dz_2/task_2_4.py
+++ Morozov_Kirill_dz_2/task_2_4.py
@@ -1,10 +1,3 @@
-def convert_name_extract(list_in: list) -> list:
-    """Извлекает имена из элементов и формирует список приветствий."""
-    # пишите реализацию своей программы здесь
-    list_out = ["здесь должены оказаться результирующие строковые приветствия"]
-    return list_out
-
-
 my_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
-result = convert_name_extract(my_list)
-print(result)
+for element in my_list:
+    print('Привет,', element.split()[-1].title(), '!')
