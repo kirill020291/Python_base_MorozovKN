dab6599028ecdea8adbc5c57b06252215d871cad
kirill020291 <oooamg.snab@gmail.com>
2022-01-26 01:43:11 +0500

    задача с сортировкой списка цен

--- Morozov_Kirill_dz_2/task_2_5.py
+++ Morozov_Kirill_dz_2/task_2_5.py
@@ -1,50 +1,15 @@
-from random import uniform
+numbs = [57.8, 46.51, 97, 14.02, 18.34, 45.11, 25, 8877, 134.88, 13]
+numbs.sort()
+for numb in numbs:
+    rub = int(numb)
+    kop = (numb - int(numb)) * 100
+    print(f'{rub} руб {kop:02.0f} коп', end=', ')
+print('\n')
+numbs.reverse()
+for numb in numbs:
+    rub = int(numb)
+    kop = (numb - int(numb)) * 100
+    print(f'{rub} руб {kop:02.0f} коп', end=', ')
 
-
-def transfer_list_in_str(list_in: list) -> str:
-    """Преобразует каждый элемент списка (вещественное число) в строку вида '<r> руб <kk> коп' и
-        формирует из них единую строковую переменную разделяя значения запятой."""
-    # пишите реализацию своей программы здесь
-    str_out = "здесь итоговая строка"
-    return str_out
-
-
-my_list = [round(uniform(10, 100), 2) for _ in range(1, 16)]  # автоматическая генерация случайных 15 чисел
-print(f'Исходный список: {my_list}')
-result_1 = transfer_list_in_str(my_list)
-print(result_1)
-
-
-def sort_prices(list_in: list) -> list:
-    """Сортирует вещественные числа по возрастанию, не создавая нового списка"""
-    # пишите реализацию здесь
-    return ["отсортированный результирующий список"]
-
-
-# зафиксируйте здесь информацию по исходному списку my_list
-result_2 = sort_prices(my_list)
-# зафиксируйте здесь доказательство, что результат result_2 остался тем же объектом
-print(result_2)
-
-
-def sort_price_adv(list_in: list) -> list:
-    """Создаёт новый список и возвращает список с элементами по убыванию"""
-    # пишите реализацию здесь
-    list_out = ["список элементов в списке по убыванию"]
-    return list_out
-
-
-result_3 = sort_price_adv(my_list)
-print(result_3)
-
-
-def check_five_max_elements(list_in: list) -> list:
-    """Проверяет элементы входного списка вещественных чисел и возвращает
-        список из ПЯТИ максимальных значений"""
-    # пишите реализацию здесь
-    list_out = ["список из пяти самых больших элементов"]
-    return list_out
-
-
-result_4 = check_five_max_elements(my_list)
-print(result_4)
+# я не понял как вывести цены пяти самых дорогих товаров по возрастанию
+# и не понял как ставить точку в конце последнего значения выводимого списка
