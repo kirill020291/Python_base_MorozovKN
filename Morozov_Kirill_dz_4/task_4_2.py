cb181652650cf22484676e1a3735f27993d903c8
kirill020291 <oooamg.snab@gmail.com>
2022-02-03 23:39:32 +0500

    2ая задача

---
+++ Morozov_Kirill_dz_4/task_4_2.py
@@ -0,0 +1,43 @@
+from decimal import Decimal
+import requests
+import sys
+import typing
+from pyquery import PyQuery as pq
+import lxml
+
+URL = 'http://www.cbr.ru/scripts/XML_daily.asp'
+
+
+def send_request() -> requests.Response:
+    """Выполняет запрос данных с ЦБР"""
+    response = requests.get(URL, timeout=2)
+    if not response.ok:
+        print(f'Запрос не успешен: {response.status_code}')
+        sys.exit(0)
+    return response
+
+
+def extract_data(tag: str) -> typing.List:
+    """Извлекает данные из соответствующего тега и возвращает список string значений"""
+    res = send_request()
+    main_root = pq(lxml.etree.fromstring(res.content))
+    curs_val = main_root.pop()
+    return curs_val.xpath(f'//Valute/{tag}/text()')
+
+
+def currency_rates(code: str) -> Decimal:
+    """возвращает курс валюты `code` по отношению к рублю"""
+    code = code.upper()
+    names = extract_data("CharCode")
+    if code not in names:
+        return None
+    index = names.index(code)
+    result_value = str(extract_data('Value')[index])
+    result_value = result_value.replace(',', '.')
+    result_value = Decimal(result_value)
+    result_value = result_value.quantize(Decimal('0.01'))
+    return result_value
+
+
+print(currency_rates("uSD"))
+print(currency_rates("noname"))
